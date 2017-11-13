11295699d97bc152af13547d1432f85cf809359f
Fionn Fitzmaurice <iamfionn@github.com>
2017-11-13 22:14:34 +0800

    break through nat, etc.

--- client.py
+++ client.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python3
 
 import socket
+import ipaddress
+import secret
 
 class Source:
-    def __init__(self, token, server_ip = "0.0.0.0", server_port = 10000):
+
+    def __init__(self, token, server_ip, server_port):
         self.token = token
         self.server_address = (server_ip, server_port)
-        self.local_address = ("0.0.0.0", server_port + 1)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(("", 0))
 
-    def send(self, token = None):
-        token = token or self.token
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        sock.sendto(token, self.server_address)
-        print("Sent", token.decode(), "to %s:%s" % self.server_address)
+    def send(self):
+        self.sock.sendto(self.token, self.server_address)
+        print("Sent \"{}\" to {}:{}".format(self.token.decode(), *self.server_address))
 
     def _receive(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind(self.local_address)
-
-        data = sock.recvfrom(4096)[0]
-        return data.decode().split(" ")
+        print("Listening on %s:%s" % self.sock.getsockname())
+        data = self.sock.recvfrom(4096)[0]
+        return set([ipaddress.ip_address(ip) for ip in data.decode().split()])
 
     def query(self):
-        self.send(self.token + b'?')
+        self.sock.sendto(self.token + b'?', self.server_address)
+        print("Getting IPs for \"{}\"".format(self.token.decode()))
         return self._receive()
 
 if __name__ == "__main__":
-    token = b"YELLOW SUBMARINE"
-    source = Source(token)
+    source = Source(secret.token, secret.server_ip, secret.server_port)
 
     source.send()
     ips = source.query()
-    print(ips)
+
+    for ip in ips:
+        print(ip)
 
--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import socket
-import sys
 import time
 from collections import namedtuple
 
@@ -9,28 +8,27 @@ class Sink:
 
     TOKEN_LENGTH = 16
 
-    def __init__(self):
+    def __init__(self, port = 10000):
         self.known_clients = {}
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(("", port))
 
     def add_client(self, client):
-        # TODO: accommodate client.token + b'?' here.
-        if len(client.token) == self.TOKEN_LENGTH:
-            if client.token not in self.known_clients:
-                self.known_clients = {client.token: set([client.ip])}
+        if len(client.token) >= self.TOKEN_LENGTH:
+            token = client.token[:self.TOKEN_LENGTH]
+            if token not in self.known_clients:
+                self.known_clients = {token: set([client.ip])}
             else:
-                self.known_clients[client.token].update([client.ip])
+                self.known_clients[token].update([client.ip])
 
     def listen(self):
         Client = namedtuple("Client", ["token", "ip", "port"])
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        server_address = ('0.0.0.0', 10000)
-        sock.bind(server_address)
-        print("Listening on %s:%s" % server_address)
+        print("Listening on %s:%s" % self.sock.getsockname())
 
         while True:
-            data, address = sock.recvfrom(4096)
-            print("Received", data.decode(), "from", address[0])
-            client = Client(data, address[0], address[1])
+            data, address = self.sock.recvfrom(1024)
+            print("Received \"{}\" from {}:{}".format(data.decode(), *address))
+            client = Client(data, *address)
             self.add_client(client)
             self.respond(client)
 
@@ -42,13 +40,11 @@ class Sink:
         """
         if len(client.token) == self.TOKEN_LENGTH + 1 and client.token[-1] == 63:
             ips = bytes(" ".join(self.known_clients[client.token[:-1]]), "ascii")
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             # We sleep here because the client is (probably) synchronous.
             time.sleep(0.1)
-            port = 10001
-            sock.sendto(ips, (client.ip, port))
-            print("Sent {} to {}:{}".format(ips.decode(), client.ip, port))
-        
+            self.sock.sendto(ips, (client.ip, client.port))
+            print("Sent \"{}\" to {}:{}".format(ips.decode(), client.ip, client.port))
 
 if __name__ == "__main__":
     Sink().listen()
+
